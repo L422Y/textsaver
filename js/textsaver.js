f338f9ffb75cc17840a370425a73801f4f927889
Larry Williamson <lawrencealan@gmail.com>
2013-10-03 22:27:26 -0400

    don't use CSS, use canvas

--- js/textsaver.js
+++ js/textsaver.js
@@ -1,53 +1,91 @@
 var textsaver = (function (AnsiLove) {
     "use strict";
 
-    var _ansilove = AnsiLove;
-    var _files = [];
-    var _last_canvas = null;
-    var _timeout = null;
-    var _running = true;
-    var _current_time = 120;
+    var 
+        _ansilove = AnsiLove,
+        _files = [],
+        _running = true,
+        _ratio =  $(window).width() / 640,
+        _px_per_col = $(window).width()/80,
+        _px_per_row = 16,
+        _scrollPos = 0,
+        _scrollInterval,
+        _last_canvas,
+        _timeout,
+        _src_data,
+        _src_context,
+        _scrollTo,
+        _screen_canvas,
+        _screen_context,
+        _offscreen_canvas,
+        _offscreen_context;
+
+
     var opts = {
-        time_per_px: 10 //ms
-    }
+           time_per_row: 200 //ms
+        }
 
     var _go = function (files, opts) {
-        _files = files;
-        _nextRandom();
+            _files = files;
 
-    }
+
+
+            _offscreen_canvas = document.createElement('canvas');
+            _offscreen_canvas.width = 640;
+            _offscreen_canvas.height = 400;
+            _offscreen_context = _offscreen_canvas.getContext("2d");
+
+            _screen_canvas = document.createElement('canvas');
+            _screen_canvas.width = 640;
+            _screen_canvas.height = 400;
+            _screen_context = _screen_canvas.getContext("2d");
+
+            $('body').append(_screen_canvas);
+
+            _nextRandom();
+        }
 
     var _nextRandom = function () {
         var _file = _files[Math.floor(Math.random() * _files.length)];
         _ansilove.render(_file, function (canvas, sauce) {
-                console.log(sauce);
-                var _canvas = $(canvas);
-                if (_last_canvas) $(_last_canvas).fadeOut(10, function () {
-                    $(this).remove();
-                });
-                _canvas.hide().appendTo($('body'));
-                _canvas.css('top', $(window).height()).show();
-                clearTimeout(_timeout);
-                _last_canvas = _canvas;
-                _current_time = _canvas.height() * opts.time_per_px;
-
-                _scrollIt(_canvas);
-                _timeout = setTimeout(_nextRandom, _current_time);
-
+                _last_canvas = canvas;
+                _src_context = canvas.getContext("2d");
+                _src_data = _src_context.getImageData(0, 0, _src_context.canvas.width, _src_context.canvas.height);
+                _scrollIt(canvas);
             },
             {"font": "80x25", "bits": "8", "icecolors": 1, "columns": 80});
 
     }
+    
+    var _scrollIt_canvas;
+    var _window_height = $(window).height();
+    var _old_data = null;
+    var _old_offset = 0;
 
     var _scrollIt = function ($canvas) {
-        var _scrollTo = $canvas.height();
-        $($canvas).animate({top: -_scrollTo}, {
-            duration: _current_time,
-            step: function (now, fx) {
-                fx.now = parseInt(now /16) * 16;
-            },
-            easing: 'linear'
-        });
+        _window_height = $(window).height();
+        _scrollPos = 400;
+        _scrollTo = -$canvas.height + _screen_canvas.height;
+        _scrollIt_canvas = $canvas;
+        _scrollIt_next();
+    }
+
+    var _scrollIt_next = function () {
+        _scrollPos-=_px_per_row;
+        if(_scrollPos<=_scrollTo){
+            _old_data = _offscreen_context.getImageData(0, 0, _src_context.canvas.width, _src_context.canvas.height, 0, _scrollPos);
+          _nextRandom();
+          _old_offset = -_px_per_row;
+          return  
+        } 
+        _old_offset-=_px_per_row;
+        if(_old_data)_offscreen_context.putImageData(_old_data,0,_old_offset);
+        _offscreen_context.putImageData(_src_data, 0, _scrollPos);
+        _screen_context.drawImage(_offscreen_canvas, 0, 0);
+
+        clearTimeout(_scrollInterval);
+        //* (_scrollPos>20?_scrollPos/_scrollTo:1)
+        _scrollInterval = setTimeout(_scrollIt_next,opts.time_per_row);
     }
 
     return {
